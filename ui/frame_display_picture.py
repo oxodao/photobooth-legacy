25cf2a98ecfabe8f6de3252b9351642f6a845336
Nathan <nathan@janczewski.fr>
2021-09-08 20:00:02 +0200

    adding scaling

--- ui/frame_display_picture.py
+++ ui/frame_display_picture.py
@@ -1,5 +1,6 @@
 import pygame
 from .frame import Frame
+from utils.config import *
 
 class FrameDisplayPicture(Frame):
     def __init__(self, pbui, root_surface, picture_path):
@@ -12,12 +13,20 @@ class FrameDisplayPicture(Frame):
         self.picture_path = picture_path
 
         self.frame_picture = pygame.image.load(r'assets/frame.png')
+        original_size = self.frame_picture.get_size()
         self.frame_picture = pygame.transform.smoothscale(self.frame_picture, pbui.size)
+
+        self.scaled_to = get_frame_borders_scaled(original_size)
+
+        w = pbui.size[0] - (self.scaled_to[0] + self.scaled_to[2])
+        h = pbui.size[1] - (self.scaled_to[1] + self.scaled_to[3])
+
         self.last_picture = pygame.image.load(picture_path)
+        self.last_picture = pygame.transform.smoothscale(self.last_picture, (w, h))
 
     def render(self):
 
-        self._root_surface.blit(self.last_picture, (0, 0))
+        self._root_surface.blit(self.last_picture, (self.scaled_to[0], self.scaled_to[1]))
         self._root_surface.blit(self.frame_picture, (0, 0))
 
         now = pygame.time.get_ticks()
--- utils/config.py
+++ utils/config.py
@@ -1,3 +1,16 @@
 RESOLUTION = (960, 540)
 CAMERA_RESOLUTION = (1280, 720)
 REFRESH_TIMEOUT = 50
+
+FRAME_BORDERS = (120, 120, 120, 120)
+
+def get_frame_borders_scaled(original_frame_size):
+    ratioX = RESOLUTION[0] / original_frame_size[0]
+    ratioY =  RESOLUTION[1] / original_frame_size[1]
+
+    return (
+        int(FRAME_BORDERS[0] * ratioX),
+        int(FRAME_BORDERS[1] * ratioY),
+        int(FRAME_BORDERS[2] * ratioX),
+        int(FRAME_BORDERS[3] * ratioY)
+    )
